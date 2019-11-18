289f864e2a809d25b2b0310334f60b527b5ef053
Hisham <hiqb217@gmail.com>
2019-11-18 11:35:27 +0000

    First commit

---
+++ source/twoSum.py
@@ -0,0 +1,14 @@
+#Problem 1
+#Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+def twoSum(argument1, target):
+    print("two sum function")
+
+def main():
+    print("hello world")
+    twoSum(0,0)
+
+##################################
+
+main()
