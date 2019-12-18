2dfad41c42d431059797a7dffe34d9c6017d61d6
Stephen Laws <sl6114@dyn111-232.ib.ic.ac.uk>
2019-12-18 14:46:50 +0000

    Solution to problem 1

--- source/twoSum.py
+++ source/twoSum.py
@@ -2,13 +2,25 @@
 #Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 #You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-def twoSum(argument1, target):
-    print("two sum function")
+"""
+Given nums = [2, 7, 11, 15], target = 9,
 
-def main():
-    print("hello world")
-    twoSum(0,0)
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+"""
 
-##################################
 
-main()
+def twoSum(nums, target):
+    dict1 = {}  #dictionary magic
+    for i in range(len(nums)):
+        if nums[i] in dict1:
+            return [dict1[nums[i]], i]
+        else:
+            dict1[target-nums[i]] = i
+
+
+# Test case
+nums = (4, 1, 2, 4, 3, 2)
+target = 8
+
+print(twoSum(nums, target))
